39fb8424f0e810ff27fed00782e9529f5d47a52b
Abdelmalek <m7244985@gmail.com>
2026-02-11 00:39:07 +0200

    sources and news data APIs

---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/api_manager.dart';
+import 'package:news_app/screens/home_screen/widgets/news_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/';
+
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("home"), centerTitle: true),
+      body: FutureBuilder(
+        future: ApiManager.getSources(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == .waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text("some thing went wrong");
+          } else {
+            var dataList = snapshot.data?.sources ?? [];
+            return Column(
+              children: [
+                DefaultTabController(
+                  initialIndex: selectedIndex,
+                  length: dataList.length,
+                  child: TabBar(
+                    onTap: (value) => setState(() {
+                      selectedIndex = value;
+                    }),
+                    isScrollable: true,
+                    tabAlignment: .start,
+                    indicatorColor: Colors.black,
+                    dividerColor: Colors.transparent,
+                    tabs: dataList
+                        .map((e) => Tab(child: Text(e.name ?? "")))
+                        .toList(),
+                  ),
+                ),
+                Expanded(
+                  child: NewsScreen(
+                    sourcesID: dataList[selectedIndex].id ?? '',
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen/widgets/news_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/api_manager.dart';
+
+class NewsScreen extends StatelessWidget {
+  final String sourcesID;
+
+  const NewsScreen({super.key, required this.sourcesID});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: ApiManager.getNewsData(sourcesID),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == .waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text("some thing went wrong");
+        } else {
+          var articles = snapshot.data?.articles ?? [];
+          return articles.isEmpty
+              ? Center(child: Text("No data"))
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            articles[index].urlToImage ?? '',
+                            height: 200,
+                            width: double.infinity,
+                            fit: .cover,
+                          ),
+                          Text(
+                            articles[index].title ?? "",
+                            maxLines: 1,
+                            overflow: .ellipsis,
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            articles[index].description ?? "",
+                            maxLines: 2,
+                            overflow: .ellipsis,
+                          ),
+                          SizedBox(height: 10),
+
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Text(articles[index].author ?? ""),
+                              // Text(
+                              //   articles[index].author?.split(" ").first ?? "",
+                              // ),
+                              Text(
+                                articles[index].publishedAt?.substring(0, 10) ??
+                                    "",
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: articles.length,
+                );
+        }
+      },
+    );
+  }
+}
